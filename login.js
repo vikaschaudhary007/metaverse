1b56e08cfc2dd11ee615dc24efac4fedddf44a48
Rita0156 <ritachaudhari006@gmail.com>
2022-11-30 14:28:24 +0530

    code check

--- login.js
+++ login.js
@@ -1,2 +1,11 @@
 let login= "vikas";
 let password= "vikas123";
+let reg_login="vikas";
+let reg_pass="vikas123"
+if(reg_login==login){
+  if(reg_pass==password){
+     console.log("Login Successfull")
+  }
+} else{
+ console.log("Wrong password or email")
+}
